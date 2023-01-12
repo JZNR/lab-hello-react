3696a84d39134ba53bed18662858baf2f7f8e3e9
Jose Zuccon <joseff.zuccon@gmail.com>
2023-01-12 21:45:18 +0000

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,13 +9,15 @@ function App() {
           <img src="./images/package-lock.png" alt="logo" />
           <img src="./images/menu-top-xs.png" alt="menu"/>
         </div>
-        <div>
+        <div className="cool-stuff">
           <h1>Say Hello to ReactJS</h1>
-          <p>You will learn how to use the most popular frontend library, and become a super Ninja Developer</p>
+          <div>
+            <p>You will learn how to use the most popular frontend library, and become a super Ninja Developer</p>
+          </div>
           <a>Awesome!</a>
         </div>
       </div>
-      <div>
+      <div className="images">
           <img src="./images/icon1.png" alt="logo" />
           <img src="./images/icon2.png" alt="logo" />
           <img src="./images/icon3.png" alt="logo" />
