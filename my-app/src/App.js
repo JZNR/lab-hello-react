92a8a6b5836976a1bd8c2083b316f746c125b876
Jose Zuccon <joseff.zuccon@gmail.com>
2023-01-12 21:36:13 +0000

    done

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grande-div">
+        <div className="menu">
+          <img src="./images/package-lock.png" alt="logo" />
+          <img src="./images/menu-top-xs.png" alt="menu"/>
+        </div>
+        <div>
+          <h1>Say Hello to ReactJS</h1>
+          <p>You will learn how to use the most popular frontend library, and become a super Ninja Developer</p>
+          <a>Awesome!</a>
+        </div>
+      </div>
+      <div>
+          <img src="./images/icon1.png" alt="logo" />
+          <img src="./images/icon2.png" alt="logo" />
+          <img src="./images/icon3.png" alt="logo" />
+          <img src="./images/icon4.png" alt="logo" />
+      </div>
+    </div>
+  );
+}
+export default App;
